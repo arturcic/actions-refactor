4dcff564d38a3d2bb6f1b2dc882172f12afa4092
Artur Stolear <arturcic@gmail.com>
2023-09-04 21:58:03 +0200

    wip

--- src/tools/gitversion/main.ts
+++ src/tools/gitversion/main.ts
@@ -1,5 +1,23 @@
-const agent = await import('../../agents/azure/gitversion/index.js')
+import {parseArgs} from "node:util";
 
+const {
+    values: {name, cool},
+} = parseArgs({
+    options: {
+        name: {
+            type: "string",
+            short: "n",
+        },
+        cool: {
+            type: "boolean",
+            short: "c",
+        },
+    },
+});
+
+const agent = await import('@agents/azure')
+
+console.log(`${name} is ${cool ? "cool" : "not cool"}`);
 console.log('Hello from agent: ' + agent.BuildAgent.name)
 
-export { }
+export {}
--- src/vite.config.ts
+++ src/vite.config.ts
@@ -6,7 +6,27 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     build: {
         rollupOptions: {
-            logLevel: 'debug',
+            external: [
+                "node:util",
+                "node:buffer",
+                "node:stream",
+                "node:net",
+                "node:url",
+                "node:fs",
+                "node:path",
+                "perf_hooks",
+            ],
+            output: {
+                globals: {
+                    "node:stream": "stream",
+                    "node:buffer": "buffer",
+                    "node:util": "util",
+                    "node:net": "net",
+                    "node:url": "url",
+                    perf_hooks: "perf_hooks",
+                },
+                // inlineDynamicImports: true,
+            },
         },
         target: 'esnext',
         lib: {
@@ -17,4 +37,10 @@ export default defineConfig({
         },
     },
     plugins: [/*dts()*/],
+    resolve: {
+        alias: {
+            '@agents/azure': resolve(__dirname, 'agents/azure'),
+            '@agents/github': resolve(__dirname, 'agents/github'),
+        }
+    },
 });
