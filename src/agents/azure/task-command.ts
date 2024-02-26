a758d9275230856e87bafb600b9de7bd4695fcc1
Artur Stolear <artur.stolear@gmail.com>
2024-02-27 00:34:12 +0100

    remove azure-pipelines-task-lib

--- src/agents/azure/task-command.ts
+++ src/agents/azure/task-command.ts
@@ -55,63 +55,10 @@ export class TaskCommand {
     }
 }
 
-export function commandFromString(commandLine: string): TaskCommand {
-    const lbPos = commandLine.indexOf('[')
-    const rbPos = commandLine.indexOf(']')
-    if (lbPos === -1 || rbPos === -1 || rbPos - lbPos < 3) {
-        throw new Error('Invalid command brackets')
-    }
-    const cmdInfo = commandLine.substring(lbPos + 1, rbPos)
-    const spaceIdx = cmdInfo.indexOf(' ')
-
-    let command = cmdInfo
-    const properties: { [key: string]: string } = {}
-
-    if (spaceIdx > 0) {
-        command = cmdInfo.trim().substring(0, spaceIdx)
-        const propSection = cmdInfo.trim().substring(spaceIdx + 1)
-
-        const propLines: string[] = propSection.split(';')
-        for (let propLine of propLines) {
-            propLine = propLine.trim()
-            if (propLine.length > 0) {
-                const eqIndex = propLine.indexOf('=')
-                if (eqIndex === -1) {
-                    throw new Error(`Invalid property: ${propLine}`)
-                }
-
-                const key: string = propLine.substring(0, eqIndex)
-                const val: string = propLine.substring(eqIndex + 1)
-
-                properties[key] = unescape(val)
-            }
-        }
-    }
-
-    const msg: string = unEscapeData(commandLine.substring(rbPos + 1))
-    return new TaskCommand(command, properties, msg)
-}
-
 function escapeData(s: string): string {
     return s.replace(/%/g, '%AZP25').replace(/\r/g, '%0D').replace(/\n/g, '%0A')
 }
 
-function unEscapeData(s: string): string {
-    return s
-        .replace(/%0D/g, '\r')
-        .replace(/%0A/g, '\n')
-        .replace(/%AZP25/g, '%')
-}
-
 function escape(s: string): string {
     return s.replace(/%/g, '%AZP25').replace(/\r/g, '%0D').replace(/\n/g, '%0A').replace(/]/g, '%5D').replace(/;/g, '%3B')
-}
-
-function unescape(s: string): string {
-    return s
-        .replace(/%0D/g, '\r')
-        .replace(/%0A/g, '\n')
-        .replace(/%5D/g, ']')
-        .replace(/%3B/g, ';')
-        .replace(/%AZP25/g, '%')
 }
