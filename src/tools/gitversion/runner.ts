b45e906baa6999471eff7385ce52e5b8fcb80b80
Artur <arturcic@gmail.com>
2024-03-13 02:54:15 +0100

    backport code from original repo

--- src/tools/gitversion/runner.ts
+++ src/tools/gitversion/runner.ts
@@ -1,12 +1,12 @@
 import { IBuildAgent } from '@agents/common'
-import { Commands } from './models'
+import { Commands, GitVersionOutput } from './models'
 import { GitVersionTool } from './tool'
 
 export class Runner {
     private readonly gitVersionTool: GitVersionTool
 
-    constructor(private readonly agent: IBuildAgent) {
-        this.gitVersionTool = new GitVersionTool(this.agent)
+    constructor(private readonly buildAgent: IBuildAgent) {
+        this.gitVersionTool = new GitVersionTool(this.buildAgent)
     }
 
     async run(command: Commands): Promise<number> {
@@ -20,20 +20,20 @@ export class Runner {
 
     private async setup(): Promise<number> {
         try {
-            this.agent.info(`Running on: '${this.agent.agentName}'`)
-            this.agent.debug('Disabling telemetry')
+            this.buildAgent.info(`Running on: '${this.buildAgent.agentName}'`)
+            this.buildAgent.debug('Disabling telemetry')
             this.gitVersionTool.disableTelemetry()
 
-            this.agent.debug('Installing GitVersion')
+            this.buildAgent.debug('Installing GitVersion')
             const toolPath = await this.gitVersionTool.install()
 
-            this.agent.info(`Set GITVERSION_PATH to ${toolPath}`)
-            this.agent.setVariable('GITVERSION_PATH', toolPath)
+            this.buildAgent.info(`Set GITVERSION_PATH to ${toolPath}`)
+            this.buildAgent.setVariable('GITVERSION_PATH', toolPath)
             return 0
         } catch (error) {
             console.log(error)
             if (error instanceof Error) {
-                this.agent.setFailed(error?.message, true)
+                this.buildAgent.setFailed(error?.message, true)
             }
             return -1
         }
@@ -41,46 +41,46 @@ export class Runner {
 
     private async execute(): Promise<number> {
         try {
-            this.agent.debug(`Agent: '${this.agent.agentName}'`)
-            this.agent.debug('Disabling telemetry')
+            this.buildAgent.debug(`Agent: '${this.buildAgent.agentName}'`)
+            this.buildAgent.debug('Disabling telemetry')
             this.gitVersionTool.disableTelemetry()
 
-            this.agent.info('Executing GitVersion')
+            this.buildAgent.info('Executing GitVersion')
             const result = await this.gitVersionTool.run()
 
             if (result.code === 0) {
-                this.agent.info('GitVersion executed successfully')
+                this.buildAgent.info('GitVersion executed successfully')
                 const { stdout } = result
 
-                this.agent.info('GitVersion output:')
-                this.agent.info('-------------------')
-                this.agent.info(stdout)
-                this.agent.info('-------------------')
+                this.buildAgent.info('GitVersion output:')
+                this.buildAgent.info('-------------------')
+                this.buildAgent.info(stdout)
+                this.buildAgent.info('-------------------')
 
-                this.agent.debug('Parsing GitVersion output')
+                this.buildAgent.debug('Parsing GitVersion output')
                 if (stdout.lastIndexOf('{') === -1 || stdout.lastIndexOf('}') === -1) {
-                    this.agent.debug('GitVersion output is not valid JSON')
-                    this.agent.setFailed('GitVersion output is not valid JSON', true)
+                    this.buildAgent.debug('GitVersion output is not valid JSON')
+                    this.buildAgent.setFailed('GitVersion output is not valid JSON', true)
                     return -1
                 } else {
                     const jsonOutput = stdout.substring(stdout.lastIndexOf('{'), stdout.lastIndexOf('}') + 1)
 
-                    const gitVersionOutput = JSON.parse(jsonOutput)
+                    const gitVersionOutput = JSON.parse(jsonOutput) as GitVersionOutput
                     this.gitVersionTool.writeGitVersionToAgent(gitVersionOutput)
-                    this.agent.setSucceeded('GitVersion executed successfully', true)
+                    this.buildAgent.setSucceeded('GitVersion executed successfully', true)
                     return 0
                 }
             } else {
-                this.agent.debug('GitVersion failed')
+                this.buildAgent.debug('GitVersion failed')
                 const error = result.error
                 if (error instanceof Error) {
-                    this.agent.setFailed(error?.message, true)
+                    this.buildAgent.setFailed(error?.message, true)
                 }
                 return -1
             }
         } catch (error) {
             if (error instanceof Error) {
-                this.agent.setFailed(error?.message, true)
+                this.buildAgent.setFailed(error?.message, true)
             }
             return -1
         }
